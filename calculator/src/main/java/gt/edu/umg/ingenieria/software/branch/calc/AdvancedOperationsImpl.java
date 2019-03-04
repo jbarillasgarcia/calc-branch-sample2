49bdda23da3139f48fd11dfaea173a9a0a2ceac2
abarillas <abarillas@nbwaudev04>
2019-03-04 12:00:31 -0600

    Added AdvancedOperations implementation.

---
+++ calculator/src/main/java/gt/edu/umg/ingenieria/software/branch/calc/AdvancedOperationsImpl.java
@@ -0,0 +1,50 @@
+package gt.edu.umg.ingenieria.software.branch.calc;
+
+public class AdvancedOperationsImpl implements AdvancedOperations {
+
+    @Override
+    public long factorial(int n) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int mod(int dividendo, int divisor) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long mod(long dividendo, long divisor) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long sqr(int base) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long sqr(long base) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long pow(long base, int exponent) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long sqrt(int a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public long abs(int a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double pi() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
