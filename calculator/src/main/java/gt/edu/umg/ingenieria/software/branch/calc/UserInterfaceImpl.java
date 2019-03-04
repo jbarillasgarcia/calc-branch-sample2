04e0e3e0aa823de082fa68f68b31967878830f06
abarillas <abarillas@nbwaudev04>
2019-03-04 12:04:29 -0600

    Added main method.

--- calculator/src/main/java/gt/edu/umg/ingenieria/software/branch/calc/UserInterfaceImpl.java
+++ calculator/src/main/java/gt/edu/umg/ingenieria/software/branch/calc/UserInterfaceImpl.java
@@ -2,4 +2,8 @@ package gt.edu.umg.ingenieria.software.branch.calc;
 
 public class UserInterfaceImpl implements UserInterface {
     
+    public static void main(String[] args) {
+        System.out.println("Hello world from Calc App!!!");
+    }
+    
 }
